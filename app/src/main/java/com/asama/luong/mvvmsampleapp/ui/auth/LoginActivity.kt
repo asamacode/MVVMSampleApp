56b8b79b4d84cb35c26ae6492030fbaa8b9aaa7b
A Sama <channelasama@gmail.com>
2019-10-11 15:35:56 +0700

    project setup

---
+++ app/src/main/java/com/asama/luong/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.asama.luong.mvvmsampleapp.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asama.luong.mvvmsampleapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

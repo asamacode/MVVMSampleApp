8f9be54569717cd94ab48a216936e57ae20850d9
A Sama <channelasama@gmail.com>
2019-10-12 09:55:34 +0700

    data bingding

--- app/src/main/java/com/asama/luong/mvvmsampleapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/asama/luong/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -2,12 +2,37 @@ package com.asama.luong.mvvmsampleapp.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.asama.luong.mvvmsampleapp.R
+import com.asama.luong.mvvmsampleapp.databinding.ActivityLoginBinding
+import com.asama.luong.mvvmsampleapp.util.toast
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), AuthListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
+
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
     }
+
+    override fun onStarted() {
+        toast("Login Started")
+    }
+
+    override fun onSuccess() {
+        toast("Login Success")
+    }
+
+    override fun onFailure(message: String) {
+        toast(message)
+    }
+
 }

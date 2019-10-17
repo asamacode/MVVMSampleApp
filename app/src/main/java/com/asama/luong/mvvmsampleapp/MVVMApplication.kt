9292ea05ef76fc2461b9a77b7e682dd3dddfe9ee
A Sama <channelasama@gmail.com>
2019-10-17 15:42:39 +0700

    end

--- app/src/main/java/com/asama/luong/mvvmsampleapp/MVVMApplication.kt
+++ app/src/main/java/com/asama/luong/mvvmsampleapp/MVVMApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.asama.luong.mvvmsampleapp.data.db.AppDatabase
 import com.asama.luong.mvvmsampleapp.data.network.MyApi
 import com.asama.luong.mvvmsampleapp.data.network.NetworkConnectionInterceptor
+import com.asama.luong.mvvmsampleapp.data.preferences.PreferenceProvider
 import com.asama.luong.mvvmsampleapp.data.repositories.QuotesRepository
 import com.asama.luong.mvvmsampleapp.data.repositories.UserRepository
 import com.asama.luong.mvvmsampleapp.ui.auth.AuthViewModelFactory
@@ -24,8 +25,9 @@ class MVVMApplication : Application(), KodeinAware {
         bind() from singleton { NetworkConnectionInterceptor(instance()) }
         bind() from singleton { MyApi(instance()) }
         bind() from singleton { AppDatabase(instance()) }
+        bind() from singleton { PreferenceProvider(instance()) }
         bind() from singleton { UserRepository(instance(), instance()) }
-        bind() from singleton { QuotesRepository(instance(), instance()) }
+        bind() from singleton { QuotesRepository(instance(), instance(), instance()) }
         bind() from provider { AuthViewModelFactory(instance()) }
         bind() from provider { ProfileViewModelFactory(instance()) }
         bind() from provider { QuotesViewModelFactory(instance()) }
